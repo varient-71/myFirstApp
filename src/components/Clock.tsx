9d26cea0ed11c12dd19bb1e38d2b3e22f170e304
varient-71 <syedsaqlainahmad23@gmail.com>
2024-10-09 22:10:19 +0530

    version 1.12

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Clock({hour,minute,hands}) {
   return (
     <div className="clock">
-            <img id="colorWheel" src="public/color-wheel.jpg" alt="Color Wheel" className="color-wheel" />
+            <img id="colorWheel" src="public/color-wheel.png" alt="Color Wheel" className="color-wheel" />
             <div className="dot"></div>
             {hands && <div >
                 <div className="hour-hand" style={{transform: `rotate(${hour*30}deg)`}}></div>
