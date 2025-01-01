21eeb6638936760d5cd21cde5882bbe60eb07554
varient-71 <syedsaqlainahmad23@gmail.com>
2025-01-02 00:16:36 +0530

    rename app from 'myFirstApp' to 'Color Time'

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'myFirstApp',
+  appName: 'Color Time',
   webDir: 'dist'
 };
 
